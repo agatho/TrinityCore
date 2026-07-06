7c467ae42ff6c423fcbfb5631fbe771189793250
Shauren <shauren.trinity@gmail.com>
2026-07-06 19:37:48 +0200

    Core/Groups: Unlink group difficulty settings from player difficulty settings
    * Changing instance difficulty while in a group will no longer override it for other group members after they leave the group

--- src/server/game/Handlers/CharacterHandler.cpp
+++ src/server/game/Handlers/CharacterHandler.cpp
@@ -1456,6 +1456,8 @@ void WorldSession::HandlePlayerLogin(LoginQueryHolder const& holder)
 
     pCurrChar->GetMotionMaster()->Initialize();
     pCurrChar->SendDungeonDifficulty();
+    pCurrChar->SendRaidDifficulty(false);
+    pCurrChar->SendRaidDifficulty(true);
 
     WorldPackets::Character::LoginVerifyWorld loginVerifyWorld;
     loginVerifyWorld.MapID = pCurrChar->GetMapId();
